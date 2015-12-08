3eee62fd81e43ef910c6db0099470a48e558fda8
danielcossins11 <daniel.cossins@yahoo.com>
2015-12-08 15:48:23 -0600

    first test passing for message.cs

---
+++ MoTMe.Tests/Models/UnitTest1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MoTMe.Models;
+
+namespace MoTMe.Tests.Models
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void MessageTextAllComponents()
+        {
+            Message message = new Message();
+            message.Id = 1;
+            message.Author = "Billy";
+            message.Body = "Hi";
+            var now = DateTime.Now;
+            message.Date = now;
+
+            Assert.AreEqual(1, message.Id);
+            Assert.AreEqual("Billy", message.Author);
+            Assert.AreEqual("Hi", message.Body);
+            Assert.AreEqual(now, message.Date);
+        }
+    }
+}
---
+++ MoTMe/Models/Message.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MoTMe.Models
+{
+    public class Message
+    {
+        public int Id { get; set; }
+        public string Author { get; set; }
+        public string Body { get; set; }
+        public DateTime Date { get; set; }
+    }
+}
