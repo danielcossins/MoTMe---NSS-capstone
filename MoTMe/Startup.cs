7a03d335d6bfc62b546619556d62ba7990db7dc7
danielcossins11 <daniel.cossins@yahoo.com>
2015-12-08 14:36:56 -0600

    initial commit

---
+++ MoTMe/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MoTMe.Startup))]
+namespace MoTMe
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
