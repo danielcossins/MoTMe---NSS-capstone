593f7536f202750cb0b2df27a02e6bcf20dc33ab
danielcossins11 <daniel.cossins@yahoo.com>
2015-12-08 16:31:08 -0600

    installed entity and moq for tests

--- MoTMe.Tests/Models/MoTMeRepositoryTests.cs
+++ MoTMe.Tests/Models/MoTMeRepositoryTests.cs
@@ -1,14 +1,23 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MoTMe.Models;
+using System.Collections.Generic;
+using Moq;
+using System.Data.Entity;
+using System.Linq;
 
 namespace MoTMe.Tests.Models
 {
     [TestClass]
     public class MoTMeRepositoryTests
     {
+        private Mock<MoTMeContext> mock_context;
+        private Mock<DbSet<Message>> mock_set;
+
         [TestMethod]
-        public void Test()
+        public void TestMethod1()
         {
+
         }
     }
 }
