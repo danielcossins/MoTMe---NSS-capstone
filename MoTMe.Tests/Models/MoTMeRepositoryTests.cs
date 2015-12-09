fdf61cde019f9e36e9b420cd294db7d40e23855c
danielcossins11 <daniel.cossins@yahoo.com>
2015-12-08 20:37:37 -0600

    first few tests working for Repository

--- MoTMe.Tests/Models/MoTMeRepositoryTests.cs
+++ MoTMe.Tests/Models/MoTMeRepositoryTests.cs
@@ -12,12 +12,82 @@ namespace MoTMe.Tests.Models
     public class MoTMeRepositoryTests
     {
         private Mock<MoTMeContext> mock_context;
-        private Mock<DbSet<Message>> mock_set;
+        //private Mock<DbSet<JitterUser>> mock_set;
+        private Mock<DbSet<Message>> mock_message_set;
+        private MoTMeRepository repository;
+
+        //private void ConnectMocksToDataStore(IEnumerable<JitterUser> data_store)
+        //{
+        //    var data_source = data_store.AsQueryable<JitterUser>();
+        //    // HINT HINT: var data_source = (data_store as IEnumerable<JitterUser>).AsQueryable();
+        //    // Convince LINQ that our Mock DbSet is a (relational) Data store.
+        //    mock_set.As<IQueryable<JitterUser>>().Setup(data => data.Provider).Returns(data_source.Provider);
+        //    mock_set.As<IQueryable<JitterUser>>().Setup(data => data.Expression).Returns(data_source.Expression);
+        //    mock_set.As<IQueryable<JitterUser>>().Setup(data => data.ElementType).Returns(data_source.ElementType);
+        //    mock_set.As<IQueryable<JitterUser>>().Setup(data => data.GetEnumerator()).Returns(data_source.GetEnumerator());
+
+        //    // This is Stubbing the JitterUsers property getter
+        //    mock_context.Setup(a => a.JitterUsers).Returns(mock_set.Object);
+        //}
+
+        private void ConnectMocksToDataStore(IEnumerable<Message> data_store)
+        {
+            var data_source = data_store.AsQueryable<Message>();
+            // HINT HINT: var data_source = (data_store as IEnumerable<Jot>).AsQueryable();
+            // Convince LINQ that our Mock DbSet is a (relational) Data store.
+            mock_message_set.As<IQueryable<Message>>().Setup(data => data.Provider).Returns(data_source.Provider);
+            mock_message_set.As<IQueryable<Message>>().Setup(data => data.Expression).Returns(data_source.Expression);
+            mock_message_set.As<IQueryable<Message>>().Setup(data => data.ElementType).Returns(data_source.ElementType);
+            mock_message_set.As<IQueryable<Message>>().Setup(data => data.GetEnumerator()).Returns(data_source.GetEnumerator());
+
+            // This is Stubbing the Jots property getter
+            mock_context.Setup(a => a.Messages).Returns(mock_message_set.Object);
+        }
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            mock_context = new Mock<MoTMeContext>();
+            //mock_set = new Mock<DbSet<MoTMeUser>>();
+            mock_message_set = new Mock<DbSet<Message>>();
+            repository = new MoTMeRepository(mock_context.Object);
+        }
+
+        [TestCleanup]
+        public void Cleanup()
+        {
+            mock_context = null;
+            //mock_set = null;
+            mock_message_set = null;
+            repository = null;
+        }
 
         [TestMethod]
-        public void TestMethod1()
+        public void MoTMeContextEnsureICanCreateInstance()
         {
+            MoTMeContext context = mock_context.Object;
+            Assert.IsNotNull(context);
+        }
 
+        [TestMethod]
+        public void JitterRepositoryEnsureICanGetAllMessages()
+        {
+            // Arrange
+            var expected = new List<Message>
+            {
+                new Message {Body = "Hello" },
+                new Message { Body = "Hello2"}
+            };
+            mock_message_set.Object.AddRange(expected);
+
+            ConnectMocksToDataStore(expected);
+
+            // Act
+            var actual = repository.GetAllMessages();
+            // Assert
+
+            Assert.AreEqual("Hello", actual.First().Body);
+            CollectionAssert.AreEqual(expected, actual);
         }
     }
 }
--- MoTMe/Models/MoTMeRepository.cs
+++ MoTMe/Models/MoTMeRepository.cs
@@ -8,6 +8,23 @@ namespace MoTMe.Models
 {
     public class MoTMeRepository
     {
-        
+        private MoTMeContext _context;
+        public MoTMeContext Context { get { return _context; } }
+
+        public MoTMeRepository()
+        {
+            _context = new MoTMeContext();
+        }
+
+        public MoTMeRepository(MoTMeContext a_context)
+        {
+            _context = a_context;
+        }
+
+        public List<Message> GetAllMessages()
+        {
+            var query = from messages in _context.Messages select messages;
+            return query.ToList();
+        }
     }
 }
