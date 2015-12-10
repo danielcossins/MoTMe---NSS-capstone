1ba830f7467887335f1af4db4031d92fb643e06b
danielcossins11 <daniel.cossins@yahoo.com>
2015-12-10 10:36:55 -0600

    changed structure of message and user

--- MoTMe/Models/Message.cs
+++ MoTMe/Models/Message.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -7,9 +8,17 @@ namespace MoTMe.Models
 {
     public class Message
     {
+        [Key]
         public int Id { get; set; }
-        public string Author { get; set; }
+        [Required]
+        public int AuthorId { get; set; }
+        [Required]
+        public int RecieverId { get; set; }
+        [Required]
         public string Body { get; set; }
+        [Required]
         public DateTime Date { get; set; }
+
+        public List<int> RecieverIds { get; set; }
     }
 }
--- MoTMe/Models/User.cs
+++ MoTMe/Models/User.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -7,9 +8,23 @@ namespace MoTMe.Models
 {
     public class User
     {
+        [Key]
         public int Id { get; set; }
+        [Required]
+        [MaxLength(20)]
         public string Name { get; set; }
+        [Required]
+        [MinLength(10)]
+        [MaxLength(15)]
         public int Phone { get; set; }
+
+        [MaxLength(300)]
+        public string Description { get; set; }
+        public string Photo { get; set; }
+        public string Location { get; set; }
+
+        public List<int> MessageIds { get; set; }
+        public List<int> ContactIds { get; set; }
 
         public List<Message> Messages { get; set; }
         public List<User> Contacts { get; set; }
