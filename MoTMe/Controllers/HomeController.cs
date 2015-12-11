e5b0f16adae7cd54ae04012413185af45fa6ec1c
danielcossins11 <daniel.cossins@yahoo.com>
2015-12-11 11:57:46 -0600

    made first call from database

--- MoTMe/Controllers/HomeController.cs
+++ MoTMe/Controllers/HomeController.cs
@@ -3,11 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using MoTMe.Models;
 
 namespace MoTMe.Controllers
 {
     public class HomeController : Controller
     {
+        MoTMeRepository repo = new MoTMeRepository();
+
         public ActionResult Index()
         {
             return View();
@@ -16,6 +19,9 @@ namespace MoTMe.Controllers
         public ActionResult About()
         {
             ViewBag.Message = "Your application description page.";
+            ViewBag.List = new string[] { "1", "2", "3" };
+
+            ViewBag.Thing = repo.GetMessageById(1).Body;
 
             return View();
         }
--- MoTMe/Scripts/app.js
+++ MoTMe/Scripts/app.js
@@ -2,14 +2,15 @@
 
 app.controller('Controller', ["$scope", "$http", function ($scope, $http) {
     $scope.test = "test variable";
+    console.log($scope.test);
 
-    //$scope.hello = function () {
-    //    //$scope.test = "Hello World";
+    $scope.hello = function () {
+        //$scope.test = "Hello World";
 
-    //    $http.get("/api/Test")
-    //        .success(function (data) {
-    //            $scope.test = data;
-    //        })
-    //        .error(function (error) { alert(error.error) });
-    //}
+        $http.get("/api/Messages/1")
+            .success(function (data) {
+                console.log(data);
+            })
+            .error(function (error) { alert(error.error) });
+    }
 }]);
