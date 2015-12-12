07ee80d6ee5236fc82c6c06db68857a311ceb2c7
danielcossins11 <daniel.cossins@yahoo.com>
2015-12-11 23:14:40 -0600

    calling c# method from angular

--- MoTMe/Scripts/app.js
+++ MoTMe/Scripts/app.js
@@ -3,4 +3,15 @@
 app.controller('Controller', ["$scope", "$http", function ($scope, $http) {
     $scope.test = "test variable";
     console.log($scope.test);
+
+    
+    $http({
+        url: "/MoTMeRepository/GetMessageById",
+        method: "GET",
+        params: { id: 1 }
+    }).success(function (data) {
+        console.log(data);
+    }).error(function (error) {
+        console.log(error);
+    });
 }]);
